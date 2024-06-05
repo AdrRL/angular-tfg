5fd40867cee4fd66093af2d4ec277467f1498873
Adrián Rodríguez <Adrian.Rodriguez@indivasaas.com>
2024-06-05 17:10:54 +0200

    Actualizacion pestaña principal

--- src/app/views/general-page/general-page.component.ts
+++ src/app/views/general-page/general-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'src/app/services/cookie.service';
 
 
 @Component({
@@ -11,8 +13,24 @@ export class GeneralPageComponent
 
   constructor
   (
+    private router: Router,
+    private cookieService: CookieService,
   )
   {
+  }
+
+  public goTo(): void
+  {
+    if (this.checkCookie())
+      this.router.navigate(['/FPAwithOpenAI/principal']);
+    else
+      this.router.navigate(['/FPAwithOpenAI/acceso']);
+
+  }
+
+  public checkCookie(): string
+  {
+    return this.cookieService.getCookie("email");
   }
 
 
